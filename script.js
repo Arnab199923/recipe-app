116f02a6fcf7c965876f1c48f1f148e65489cf80
Arnab199923 <96194042+Arnab199923@users.noreply.github.com>
2023-09-09 22:36:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const searchBox = document.querySelector(".searchBox");
+const searchButton = document.querySelector(".searchButton");
+const recipeContainer = document.querySelector(".recipeContainer");
+
+
+
+const fetchrecipe = async (query)=>{
+    recipeContainer.innerHTML="getting your recipe hang on 😍";
+    const data =await fetch( `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
+    
+
+    recipeContainer.innerHTML="";
+    response.meals.forEach(meal => {
+        console.log(meal);
+
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add("recipe");
+        recipeDiv.innerHTML = `<img src ="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p>${meal.strTags}</p>
+        <p>${meal.strCategory}</p>
+        
+        `
+        recipeContainer.appendChild(recipeDiv);
+        
+    });
+    
+}
+
+
+searchButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const searchInput = searchBox.value.trim();
+    fetchrecipe(searchInput);
+    // console.log("helo"); 
+});
+
